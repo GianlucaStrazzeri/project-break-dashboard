2ac644d49e464fc3039216f1d2ca42a42333ad57
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-13 22:03:29 +0100

    Modificado css del documento

--- scripts/Enlaces.js
+++ scripts/Enlaces.js
@@ -2,3 +2,5 @@ let nombre=document.getElementById("nombre");
 console.log(nombre);
 let enlace=document.getElementById("enlace");
 console.log(enlace);
+let ruta=document.getElementById("ruta");
+console.log(ruta);
