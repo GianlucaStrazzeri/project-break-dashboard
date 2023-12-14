917705b7fd7fd4f4b87059f8b054ba0b5459c7a4
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-14 11:53:30 +0100

    Cambiado en passwoord el tipo de entrada a select y añadido hasta 12 valores

--- scripts/Enlaces.js
+++ scripts/Enlaces.js
@@ -1,24 +1,22 @@
-let nombreEnlace=document.getElementById("nombre");
-let ruta=document.getElementById("ruta");
+//Guardo las variables con su id desde el html
 let guardarEnlace=document.getElementById("guardarEnlacesButton");
 let areaparasalvarenlaces=document.getElementById("areaparasalvarenlaces");
 
 
-console.log(nombreEnlace,guardarEnlace, ruta);
-
-//localStorage.clear();
-localStorage.setItem(`nombreWeb`,"vacio");
-localStorage.setItem(`rutaWeb`,"http://");
-
-let myStoragedNameOfWeb = localStorage.getItem('nombreWeb');
-let myStoragedRutaWeb = localStorage.getItem('rutaWeb');
-
-console.log(ruta.value);
-
-//guardarEnlace.addEventlistener("click",function() {
-//    myStoragedNameOfWeb=myStoragedNameOfWeb.value,
-//    myStoragedRutaWeb=myStoragedRutaWeb.value")}
-
 guardarEnlace.addEventListener("click", function() {
-areaparasalvarenlaces.innerHTML = `<p> Nombre de la web ${myStoragedNameOfWeb}</p> 
-<p> Ruta relativa:${myStoragedRutaWeb}</p>`;})  
+    let nombreEnlace=document.getElementById("nombre");
+    let ruta=document.getElementById("ruta");
+    //creo persistencia con localstorage
+    localStorage.setItem(`nombreWeb`,nombreEnlace);
+    localStorage.setItem(`rutaWeb`,ruta);
+    let myStoragedNameOfWeb = localStorage.getItem('nombreWeb');
+    let myStoragedNameOfWebStringify= JSON.stringify(myStoragedNameOfWeb);
+    let myStoragedRutaWeb = localStorage.getItem('rutaWeb');
+    let myStoragedRutaWebStringify= JSON.stringify(myStoragedRutaWeb);
+    console.log(myStoragedNameOfWebStringify,myStoragedRutaWebStringify)
+
+areaparasalvarenlaces.innerHTML = `<p> Nombre de la web ${myStoragedNameOfWebStringify}</p> 
+<p> Ruta relativa:${myStoragedRutaWebStringify}</p>`;})  
+
+
+//JSON.stringify() pasa un objeto a un string;
