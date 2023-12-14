b7c8b250968028b86e0f0f505bb1343c45ad4e95
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-14 10:24:02 +0100

    La frase aparece al hacer click al botón de momento no captura el valor de los enlaces

--- scripts/Enlaces.js
+++ scripts/Enlaces.js
@@ -7,10 +7,18 @@ let areaparasalvarenlaces=document.getElementById("areaparasalvarenlaces");
 console.log(nombreEnlace,guardarEnlace, ruta);
 
 //localStorage.clear();
-localStorage.setItem(`nombreWeb`,"");
-localStorage.setItem(`rutaWeb`,"");
+localStorage.setItem(`nombreWeb`,"vacio");
+localStorage.setItem(`rutaWeb`,"http://");
 
 let myStoragedNameOfWeb = localStorage.getItem('nombreWeb');
 let myStoragedRutaWeb = localStorage.getItem('rutaWeb');
 
-areaparasalvarenlaces.innerHTML = `<p> Nombre de la web ${myStoragedNameOfWeb}</p> <p> Ruta relativa:${myStoragedRutaWeb}</p>`;
+console.log(ruta.value);
+
+//guardarEnlace.addEventlistener("click",function() {
+//    myStoragedNameOfWeb=myStoragedNameOfWeb.value,
+//    myStoragedRutaWeb=myStoragedRutaWeb.value")}
+
+guardarEnlace.addEventListener("click", function() {
+areaparasalvarenlaces.innerHTML = `<p> Nombre de la web ${myStoragedNameOfWeb}</p> 
+<p> Ruta relativa:${myStoragedRutaWeb}</p>`;})  
