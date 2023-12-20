d0855013bd9b4e4aa49200549152965d8fc05815
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-20 08:43:05 +0100

    modificado rutas htmls

--- scripts/Meteo.js
+++ scripts/Meteo.js
@@ -1,4 +1,33 @@
 let apiKey = ["77e828056a1d4ceea61213842231912"];
 let city= ["Alicante - Spain"];
 
-//fetch ("https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&aqi=no")
+fetch ("https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&aqi=no")
+.then((response) => {
+          if (!response.ok) {
+            throw new Error('La solicitud no fue exitosa');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+
+//Estructura fetch ejercicios
+//const obtenerChiste = document.getElementById('obtenerChiste');
+//const chiste = document.getElementById('chiste');
+
+//obtenerChiste.addEventListener('click', () => {
+//  fetch('https://v2.jokeapi.dev/joke/Any?lang=es&blacklistFlags=sexist')
+//    .then((response) => {
+//      if (!response.ok) {
+//        throw new Error('La solicitud no fue exitosa');
+//      }
+//      return response.json();
+//    })
+//    .then((data) => {
+//      chiste.innerText = data.setup + ' ' + ' ' + data.delivery;
+//    })
+//    .catch((error) => {
+//      chiste.innerText = 'No se puede  cargar el chiste';
+//    });
+//});
--- scripts/Reloj.js
+++ scripts/Reloj.js
@@ -1,4 +1,4 @@
-let reloj= document.getElementById("reloj");
+let relo= document.getElementById("relo");
 let fecha= document.getElementById("fecha");
 let frase=document.getElementById("frase");
 
@@ -81,16 +81,10 @@ const repetir = () => {
         frase.innerHTML="Buenas noches, es hora de pensar en parar y descansar"
     };
 
-
-
-
-    
-
-
     
 
     fecha.innerHTML=`Fecha: ${numeroDia}  ${mesDelAño}  ${año}`;
-    reloj.innerHTML= `&#128368; Hora: ${horas} : ${minutos} : ${segundos}`;
+    relo.innerHTML= `&#128368; Hora: ${horas} : ${minutos} : ${segundos}`;
    
    
 
@@ -100,27 +94,5 @@ const repetir = () => {
 
 setInterval(repetir, 1000);
 
-
-
-    
-
-//Explicación trabajo
-//Capturo el Id fecha del documento html y lo guardo como variable "fecha"
-//Capturo el Id reloj del documento html y lo guardo como variable "reloj"
-//el metodo new Date(permite pedir la hora fecha etc)
-//el metodo getHours(permite pedir la hora)
-//el metodo getMinutes(permite pedir los minutos)
-//el metodo getSeconds(permite pedir los segundos)
-//se utiliza setInterval para repetir la función repetir cada 1000 ms
-//se utiliza reloj.innerHTML para insertar como texto la hora , los minutos y los segundos
-//se utiliza fecha.innerHTML para insertar como texto la hora , los minutos y los segundos
-//Utilizo de comillas francesas y dollar${} para intercalar variables con html
-
-
-
-
-//Crear un evento de click  utilizando idcapturadoadd.eventlistener("click",function"){}que  
-//te cree una clase css para agregar estilos y un  elemento p con la logica dependiente del horario
-//Logica del horario
 
 
