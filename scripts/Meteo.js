f09cbce22ddc9f6858350a6cf2c731873b889b68
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-24 00:42:47 +0100

    Añadiendo información del tiempor

--- scripts/Meteo.js
+++ scripts/Meteo.js
@@ -1,8 +1,9 @@
 let zonapariciónclimatica =document.getElementById("zonapariciónclimatica");
-
 let apiKey = ["77e828056a1d4ceea61213842231912"];
 let city= ["Alicante - Spain"];
 let ciudad=document.getElementById("ciudad");
+let temperatura=document.getElementById("temperatura");
+
 
 
 const fetchData = async () => {
@@ -15,8 +16,17 @@ const fetchData = async () => {
 }
 const data = await response.json();
 console.log(data);
+//Object.values() permite sacar como array los datos de un objeto
 const locationArr= (Object.values(data.location));
-ciudad.innerHTML=locationArr[0];
+const currentArr= (Object.values(data.current));
+console.log(Object.values(data.location));
+console.log(Object.values(data.current));
+const icon=(currentArr[5].icon);
+ciudad.innerHTML=locationArr[0]+ " " + locationArr[2];
+temperatura.innerHTML=`<p> Temperatura: ${currentArr[2]}</p>
+<p> Humedad: ${currentArr[6]} % </p>
+<img src="https:${icon}"> `;
+//zonapariciónclimatica.innerHTML=`<img src=https=${}>`;
 }catch (error){
 console.error(error);
 }
