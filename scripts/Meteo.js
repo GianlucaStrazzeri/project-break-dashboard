6b19f686e469e9da3cd5a0eed66480e69a18a53b
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-23 23:56:55 +0100

    recorrdendo meteo con la función Object.values()

--- scripts/Meteo.js
+++ scripts/Meteo.js
@@ -2,28 +2,10 @@ let zonapariciónclimatica =document.getElementById("zonapariciónclimatica");
 
 let apiKey = ["77e828056a1d4ceea61213842231912"];
 let city= ["Alicante - Spain"];
+let ciudad=document.getElementById("ciudad");
 
-console.log("wei lu");
-
-//fetch('https://v2.jokeapi.dev/joke/Any?lang=es&blacklistFlags=sexist')
-//fetch (`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`)
-//.then((response) => {
-//          if (!response.ok) {
-//            throw new Error('La solicitud no fue exitosa');
-//          }
-//          return response.json();
-//        })
-//        .then((data) => {
-//          console.log(data);
-//          zonapariciónclimatica.innerText= data.setup;
- //       });
- //       .catch((error) => {
- //         zonapariciónclimatica.innerText = '<p>No se puede  cargar el chiste</p>';
- //           });
-        
 
 const fetchData = async () => {
-  
   let city= ["Alicante - Spain"];
   try{
     
@@ -33,6 +15,8 @@ const fetchData = async () => {
 }
 const data = await response.json();
 console.log(data);
+const {country, name, region} = location;
+console.log(Object.values(data.location));
 }catch (error){
 console.error(error);
 }
