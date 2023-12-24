38bccd510ed424f87583d058719fb3c5a18d2bc0
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-24 21:50:26 +0100

    Añadidos tres días

--- scripts/Meteo.js
+++ scripts/Meteo.js
@@ -1,9 +1,10 @@
                           //meteo.js
 let apiKey = ["77e828056a1d4ceea61213842231912"];
 let city= ["Alicante - Spain"];
-let meteo1día=document.getElementById("meteo1día");
-let meteoTwodays=document.getElementById("Twodays");
 let temperatura=document.getElementById("temperatura");
+let meteo1día=document.getElementById("meteo1día");
+
+
 
 const fetchData = async () => {
   let city= ["Alicante - Spain"];
@@ -14,22 +15,16 @@ const fetchData = async () => {
  throw new Error (`Error`);
 }
 const data = await response.json();
-//console.log(data);
 //Object.values() permite sacar como array los datos de un objeto
 const locationArr= (Object.values(data.location));
 const currentArr= (Object.values(data.current));
-//console.log(Object.values(data.location));
-//console.log(Object.values(data.current));
 const icon=(currentArr[5].icon);
 
 temperatura.innerHTML= `<img src="https:${icon}"> <br>
 ${locationArr[0]}   ${locationArr[2]}
 <p> Temperatura: ${currentArr[2]}</p>
 <p> Humedad: ${currentArr[6]} % </p>
-<p> Viento por Hora: ${currentArr[21]} km/h </p>
- `;
-
-
+<p> Viento por Hora: ${currentArr[21]} km/h </p>`;
 }catch (error){
 console.error(error);
 }
@@ -49,30 +44,20 @@ fetchData();
 
 
 const fetchDataTomorrow = async () => {
-   city= ["Alicante - Spain"];
-  try{
-    
- const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=2024-01-25`);
+  let today=[`2024-12-25`]
+  try{   
+ const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=${today}`);
  if (!responseTomorrow.ok) {
  throw new Error (`Error`);
 }
 const dataTomorrow = await responseTomorrow.json();
-console.log(dataTomorrow);
 const forecastArrTomorrow= (Object.values(dataTomorrow.forecast));
-
-console.log(forecastArrTomorrow);
-
-console.log(forecastArrTomorrow[0]);
 const forecastArrTomorrow0= (Object.values(forecastArrTomorrow[0]));
-console.log(forecastArrTomorrow0[0]);
-console.log(forecastArrTomorrow0[0].day.condition.icon);
-
 const iconTomorrow=(forecastArrTomorrow0[0].day.condition.icon);
-
-meteo1día.innerHTML= `<p>Mañana</p>
- <img src="https:${iconTomorrow}"> <br>`;
-
-
+meteo1día.innerHTML+= `<div class="meteo-container">
+${forecastArrTomorrow0[0].date}
+ <img src="https:${iconTomorrow}"> <br>
+ </div>`;
 }catch (error){
 console.log(error);
 }
@@ -90,34 +75,54 @@ fetchDataTomorrow();
 
 
 const fetchDataTwodays = async () => {
-  city= ["Alicante - Spain"];
- try{
-   
-const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=2024-01-25`);
-if (!responseTomorrow.ok) {
-throw new Error (`Error`);
+  let today=[`2024-12-25`]
+  try{   
+ const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=${today}`);
+ if (!responseTomorrow.ok) {
+ throw new Error (`Error`);
 }
 const dataTomorrow = await responseTomorrow.json();
-console.log(dataTomorrow);
 const forecastArrTomorrow= (Object.values(dataTomorrow.forecast));
-
-console.log(forecastArrTomorrow);
-
-console.log(forecastArrTomorrow[0]);
 const forecastArrTomorrow0= (Object.values(forecastArrTomorrow[0]));
-console.log(forecastArrTomorrow0[0]);
-console.log(forecastArrTomorrow0[0].day.condition.icon);
-
 const iconTomorrow=(forecastArrTomorrow0[0].day.condition.icon);
-
-meteoTwodays.innerHTML= ` <p> Pasado mañana</p>
- <img src="https:${iconTomorrow}"> <br>`;
-
-
+meteo1día.innerHTML+= `<div class="meteo-container">
+${forecastArrTomorrow0[0].date}
+ <img src="https:${iconTomorrow}"> <br>
+ </div>`;
 }catch (error){
 console.log(error);
 }
 }
 
 fetchDataTwodays();
+
+
+
+
+
+
+const fetchDataThreedays = async () => {
+  try{  
+ const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=2024-01-27`);
+ if (!responseTomorrow.ok) {
+ throw new Error (`Error`);
+ }
+ const dataTomorrow = await responseTomorrow.json();
+ const forecastArrTomorrow= (Object.values(dataTomorrow.forecast));
+ const forecastArrTomorrow0= (Object.values(forecastArrTomorrow[0]));
+ const iconTomorrow=(forecastArrTomorrow0[0].day.condition.icon);
+ meteo1día.innerHTML+= ` <div class="meteo-container">
+ ${forecastArrTomorrow0[0].date}
+  <img src="https:${iconTomorrow}"> <br>
+  </div>`;
+ }catch (error){
+ console.log(error);
+ }
+ }
+ 
+ fetchDataThreedays();
+
+
+
+
 
