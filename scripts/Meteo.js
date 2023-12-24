d7cbbc3c5d34ce4510250d1436dd587f2be06a1d
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-12-24 19:08:42 +0100

    Añadiendo meteo de los demás dias

--- scripts/Meteo.js
+++ scripts/Meteo.js
@@ -2,6 +2,7 @@
 let apiKey = ["77e828056a1d4ceea61213842231912"];
 let city= ["Alicante - Spain"];
 let meteo1día=document.getElementById("meteo1día");
+let meteoTwodays=document.getElementById("Twodays");
 let temperatura=document.getElementById("temperatura");
 
 const fetchData = async () => {
@@ -68,7 +69,8 @@ console.log(forecastArrTomorrow0[0].day.condition.icon);
 
 const iconTomorrow=(forecastArrTomorrow0[0].day.condition.icon);
 
-meteo1día.innerHTML= `<img src="https:${iconTomorrow}"> <br>`;
+meteo1día.innerHTML= `<p>Mañana</p>
+ <img src="https:${iconTomorrow}"> <br>`;
 
 
 }catch (error){
@@ -79,4 +81,43 @@ console.log(error);
 fetchDataTomorrow();
 
 
+
+
+
+
+
+
+
+
+const fetchDataTwodays = async () => {
+  city= ["Alicante - Spain"];
+ try{
+   
+const responseTomorrow = await fetch (`https://api.weatherapi.com/v1/future.json?key=77e828056a1d4ceea61213842231912&q=Alicante - Spain&dt=2024-01-25`);
+if (!responseTomorrow.ok) {
+throw new Error (`Error`);
+}
+const dataTomorrow = await responseTomorrow.json();
+console.log(dataTomorrow);
+const forecastArrTomorrow= (Object.values(dataTomorrow.forecast));
+
+console.log(forecastArrTomorrow);
+
+console.log(forecastArrTomorrow[0]);
+const forecastArrTomorrow0= (Object.values(forecastArrTomorrow[0]));
+console.log(forecastArrTomorrow0[0]);
+console.log(forecastArrTomorrow0[0].day.condition.icon);
+
+const iconTomorrow=(forecastArrTomorrow0[0].day.condition.icon);
+
+meteoTwodays.innerHTML= ` <p> Pasado mañana</p>
+ <img src="https:${iconTomorrow}"> <br>`;
+
+
+}catch (error){
+console.log(error);
+}
+}
+
+fetchDataTwodays();
 
